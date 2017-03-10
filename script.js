752d9273107acd0ae3455dac7b5dda4598f26100
jstubbs1980 <jayrstubbs@yahoo.com>
2017-03-09 20:08:32 -0600

    Changing cell class a bit, adding more cells to the grid, and moving CSS to head in HTML

--- script.js
+++ script.js
@@ -2,29 +2,21 @@ var CONWAYS_GAME_OF_LIFE = (function () {
 
     var gridOffset = 20;
 
-    var numberColumns = 100;
-    var numberRows = 100;
+    var numberColumns = 150;
+    var numberRows = 150;
 
-    var cellWidth = 7;
-    var cellHeight = 7;
+    var cellWidth = 5;
+    var cellHeight = 5;
 
     var gridWidth = numberColumns * cellWidth;
     var gridHeight = numberRows * cellHeight;
 
+    var Cell = function (alive) {
+        this.isAlive = alive;
+    };
+
     this.thisGeneration = [];
     this.nextGeneration = [];
-
-    var Cell = function (alive, x, y) {
-        var isAlive = alive;
-
-        this.isAlive = function () {
-            return isAlive === true;
-        };
-
-        this.setAlive = function (isLiving) {
-            isAlive = isLiving;
-        };
-    };
 
     return {
         initialize: function () {
@@ -40,12 +32,12 @@ var CONWAYS_GAME_OF_LIFE = (function () {
                     var nextGenerationCell;
 
                     if (Math.round(Math.random()) === 1) {
-                        thisGenerationCell = new Cell(true, x, y);
-                        nextGenerationCell = new Cell(true, x, y);
+                        thisGenerationCell = new Cell(true);
+                        nextGenerationCell = new Cell(true);
                     }
                     else {
-                        thisGenerationCell = new Cell(false, x, y);
-                        nextGenerationCell = new Cell(false, x, y);
+                        thisGenerationCell = new Cell(false);
+                        nextGenerationCell = new Cell(false);
                     }
 
                     thisGenerationCellColumn.push(thisGenerationCell);
@@ -61,13 +53,6 @@ var CONWAYS_GAME_OF_LIFE = (function () {
         },
 
         regenerate: function () {
-            /*
-            var clickedXCoord = event.offsetX - gridOffset;
-            var clickedYCoord = event.offsetY - gridOffset;
-
-            var xIndex = Math.floor(clickedXCoord / cellWidth);
-            var yIndex = Math.floor(clickedYCoord / cellHeight);
-            */
 
             // Any live cell with fewer than two live neighbours dies, as if caused by under-population.
             // Any live cell with two or three live neighbours lives on to the next generation.
@@ -79,20 +64,20 @@ var CONWAYS_GAME_OF_LIFE = (function () {
 
                     var numberLivingNeighbors = this.getNumberAliveNeighbors(x, y);
 
-                    if (thisGeneration[x][y].isAlive()) {
+                    if (thisGeneration[x][y].isAlive) {
                         if (numberLivingNeighbors < 2) {
-                            nextGeneration[x][y].setAlive(false);
+                            nextGeneration[x][y].isAlive = false;
                         }
                         else if (numberLivingNeighbors === 2 || numberLivingNeighbors === 3) {
-                            nextGeneration[x][y].setAlive(true);
+                            nextGeneration[x][y].isAlive = true;
                         }
                         else if (numberLivingNeighbors > 3) {
-                            nextGeneration[x][y].setAlive(false);
+                            nextGeneration[x][y].isAlive = false;
                         }
                     }
                     else {
                         if (numberLivingNeighbors === 3) {
-                            nextGeneration[x][y].setAlive(true);
+                            nextGeneration[x][y].isAlive = true;
                         }
                     }
                 }
@@ -100,7 +85,7 @@ var CONWAYS_GAME_OF_LIFE = (function () {
 
             for (var x = 0; x < numberColumns; x++) {
                 for (var y = 0; y < numberRows; y++) {
-                    thisGeneration[x][y].setAlive(nextGeneration[x][y].isAlive());
+                    thisGeneration[x][y].isAlive = nextGeneration[x][y].isAlive;
                 }
             }
 
@@ -109,13 +94,14 @@ var CONWAYS_GAME_OF_LIFE = (function () {
 
         renderGrid: function () {
             var context = document.getElementById('canvas').getContext('2d');
+
             context.clearRect(gridOffset, gridOffset, gridWidth, gridHeight);
 
             for (var x = 0; x < numberColumns; x++) {
                 var cellColumn = [];
 
                 for (var y = 0; y < numberRows; y++) {
-                    if (thisGeneration[x][y].isAlive()) {
+                    if (thisGeneration[x][y].isAlive) {
                         context.fillStyle = '#069e61';
                         context.fillRect(gridOffset + (x * cellWidth), gridOffset + (y * cellHeight), cellWidth, cellHeight)
                     }
@@ -133,23 +119,23 @@ var CONWAYS_GAME_OF_LIFE = (function () {
                     switch(y) {
                         case 0:
                             // Top left corner
-                            return thisGeneration[0][1].isAlive() +
-                                   thisGeneration[1][1].isAlive() +
-                                   thisGeneration[1][0].isAlive();
+                            return thisGeneration[0][1].isAlive +
+                                   thisGeneration[1][1].isAlive +
+                                   thisGeneration[1][0].isAlive;
                             break;
                         case numberColumns - 1:
                             // Top right corner
-                            return thisGeneration[0][numberColumns - 2].isAlive() +
-                                   thisGeneration[1][numberColumns - 2].isAlive() +
-                                   thisGeneration[1][numberColumns - 1].isAlive();
+                            return thisGeneration[0][numberColumns - 2].isAlive +
+                                   thisGeneration[1][numberColumns - 2].isAlive +
+                                   thisGeneration[1][numberColumns - 1].isAlive;
                             break;
                         default:
                             // Top row, middle
-                            return thisGeneration[0][y - 1].isAlive() +
-                                   thisGeneration[1][y - 1].isAlive() +
-                                   thisGeneration[1][y].isAlive() +
-                                   thisGeneration[1][y + 1].isAlive() +
-                                   thisGeneration[0][y + 1].isAlive();
+                            return thisGeneration[0][y - 1].isAlive +
+                                   thisGeneration[1][y - 1].isAlive +
+                                   thisGeneration[1][y].isAlive +
+                                   thisGeneration[1][y + 1].isAlive +
+                                   thisGeneration[0][y + 1].isAlive;
                             break;
                     }
                     break;
@@ -158,23 +144,23 @@ var CONWAYS_GAME_OF_LIFE = (function () {
                     switch(y) {
                         case 0:
                             // Bottom left corner
-                            return thisGeneration[numberRows - 2][0].isAlive() +
-                                   thisGeneration[numberRows - 2][1].isAlive() +
+                            return thisGeneration[numberRows - 2][0].isAlive +
+                                   thisGeneration[numberRows - 2][1].isAlive +
                                    thisGeneration[numberRows - 1][1];
                             break;
                         case numberColumns - 1:
                             // Bottom right corner
-                            return thisGeneration[numberRows - 1][numberColumns - 2].isAlive() +
-                                   thisGeneration[numberRows - 2][numberColumns - 2].isAlive() +
-                                   thisGeneration[numberRows - 2][numberColumns - 1].isAlive();
+                            return thisGeneration[numberRows - 1][numberColumns - 2].isAlive +
+                                   thisGeneration[numberRows - 2][numberColumns - 2].isAlive +
+                                   thisGeneration[numberRows - 2][numberColumns - 1].isAlive;
                             break;
                         default:
                             // Bottom row in the middle
-                            return thisGeneration[numberRows - 1][y - 1].isAlive() +
-                                   thisGeneration[numberRows - 2][y - 1].isAlive() +
-                                   thisGeneration[numberRows - 2][y].isAlive() +
-                                   thisGeneration[numberRows - 2][y + 1].isAlive() +
-                                   thisGeneration[numberRows - 1][y + 1].isAlive();
+                            return thisGeneration[numberRows - 1][y - 1].isAlive +
+                                   thisGeneration[numberRows - 2][y - 1].isAlive +
+                                   thisGeneration[numberRows - 2][y].isAlive +
+                                   thisGeneration[numberRows - 2][y + 1].isAlive +
+                                   thisGeneration[numberRows - 1][y + 1].isAlive;
                             break;
                     }
                     break;
@@ -182,28 +168,28 @@ var CONWAYS_GAME_OF_LIFE = (function () {
                 default:
                     switch(y) {
                         case 0:
-                            return thisGeneration[x - 1][0].isAlive() +
-                                   thisGeneration[x - 1][1].isAlive() +
-                                   thisGeneration[x][1].isAlive() +
-                                   thisGeneration[x + 1][1].isAlive() +
-                                   thisGeneration[x + 1][0].isAlive();
+                            return thisGeneration[x - 1][0].isAlive +
+                                   thisGeneration[x - 1][1].isAlive +
+                                   thisGeneration[x][1].isAlive +
+                                   thisGeneration[x + 1][1].isAlive +
+                                   thisGeneration[x + 1][0].isAlive;
                             break;
                         case numberColumns - 1:
-                            return thisGeneration[x - 1][numberColumns - 1].isAlive() +
-                                   thisGeneration[x - 1][numberColumns - 2].isAlive() +
-                                   thisGeneration[x][numberColumns - 2].isAlive() +
-                                   thisGeneration[x + 1][numberColumns - 2].isAlive() +
-                                   thisGeneration[x + 1][numberColumns - 1].isAlive();
+                            return thisGeneration[x - 1][numberColumns - 1].isAlive +
+                                   thisGeneration[x - 1][numberColumns - 2].isAlive +
+                                   thisGeneration[x][numberColumns - 2].isAlive +
+                                   thisGeneration[x + 1][numberColumns - 2].isAlive +
+                                   thisGeneration[x + 1][numberColumns - 1].isAlive;
                             break;
                         default:
-                            return thisGeneration[x - 1][y - 1].isAlive() +
-                                   thisGeneration[x - 1][y].isAlive() +
-                                   thisGeneration[x - 1][y + 1].isAlive() +
-                                   thisGeneration[x][y - 1].isAlive() +
-                                   thisGeneration[x][y + 1].isAlive() +
-                                   thisGeneration[x + 1][y - 1].isAlive() +
-                                   thisGeneration[x + 1][y].isAlive() +
-                                   thisGeneration[x + 1][y + 1].isAlive();
+                            return thisGeneration[x - 1][y - 1].isAlive +
+                                   thisGeneration[x - 1][y].isAlive +
+                                   thisGeneration[x - 1][y + 1].isAlive +
+                                   thisGeneration[x][y - 1].isAlive +
+                                   thisGeneration[x][y + 1].isAlive +
+                                   thisGeneration[x + 1][y - 1].isAlive +
+                                   thisGeneration[x + 1][y].isAlive +
+                                   thisGeneration[x + 1][y + 1].isAlive;
                             break;
                     }
                     break;
